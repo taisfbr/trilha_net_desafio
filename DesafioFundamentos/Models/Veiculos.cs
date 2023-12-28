fbf3a84642f1a8502289e21372a63872be389647
Helvisklei Silva <helvisklei@htomail.com>
2023-12-28 13:26:06 -0300

    Desafio concluido git statusgit status

---
+++ DesafioFundamentos/Models/Veiculos.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+/*
+DESENVOLVIMENTO FUTURO
+Vou criar uma classe veiculos para isnerir COR, Placa, Modelo,
+funcionalidade que desenvolveri no futuro, cheguei em fazer alguns teste para não alterar
+as especificações deixarei para proxima versão
+*/
+namespace DesafioFundamentos.Models
+{
+    public class Veiculos
+    {
+        private string _placa; 
+        private string _cor;
+
+        private string _modelo;
+        
+           
+
+   
+    }    
+}

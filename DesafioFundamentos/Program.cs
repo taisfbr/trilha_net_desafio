db8968d968af2558f99635ceaa41e6624b8ea0af
Fernando José <fernando.tecnicoinfo7@gmail.com>
2024-04-30 17:02:34 -0300

    Desafio COncluído

--- DesafioFundamentos/Program.cs
+++ DesafioFundamentos/Program.cs
@@ -8,10 +8,25 @@ decimal precoPorHora = 0;
 
 Console.WriteLine("Seja bem vindo ao sistema de estacionamento!\n" +
                   "Digite o preço inicial:");
-precoInicial = Convert.ToDecimal(Console.ReadLine());
+while (!decimal.TryParse(Console.ReadLine(), out precoInicial))
+{
+    Console.ForegroundColor = ConsoleColor.Yellow;
+    Console.WriteLine("== Preço inválido, para valores quebrados utilize a vírgula. Ex: R$ 1,30 => 1,3 ");
+    Console.ResetColor();
+    Console.WriteLine(
+        "Digite o preço inicial:");
+}
 
-Console.WriteLine("Agora digite o preço por hora:");
-precoPorHora = Convert.ToDecimal(Console.ReadLine());
+Console.WriteLine(
+    "Digite o preço por hora:");
+while (!decimal.TryParse(Console.ReadLine(), out precoPorHora))
+{
+    Console.ForegroundColor = ConsoleColor.Yellow;
+    Console.WriteLine("== Preço inválido, para valores quebrados utilize a vírgula. Ex: R$ 1,30 => 1,3 ");
+    Console.ResetColor();
+    Console.WriteLine(
+        "Digite o preço por hora:");
+}
 
 // Instancia a classe Estacionamento, já com os valores obtidos anteriormente
 Estacionamento es = new Estacionamento(precoInicial, precoPorHora);
@@ -23,6 +38,7 @@ bool exibirMenu = true;
 while (exibirMenu)
 {
     Console.Clear();
+    Console.WriteLine($"PREÇO INICIAL: R$ {precoInicial} | PREÇO POR HORA: R$ {precoPorHora}\n");
     Console.WriteLine("Digite a sua opção:");
     Console.WriteLine("1 - Cadastrar veículo");
     Console.WriteLine("2 - Remover veículo");

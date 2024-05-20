f16fe6ae7e94689dd38c164436c9166c387db8ab
Joao <mendoncajoao2323@gmail.com>
2024-05-20 20:34:45 -0300

    teste

--- DesafioFundamentos/Program.cs
+++ DesafioFundamentos/Program.cs
@@ -15,7 +15,7 @@ precoPorHora = Convert.ToDecimal(Console.ReadLine());
 
 // Instancia a classe Estacionamento, já com os valores obtidos anteriormente
 Estacionamento es = new Estacionamento(precoInicial, precoPorHora);
-
+string merda = "porra";
 string opcao = string.Empty;
 bool exibirMenu = true;
 

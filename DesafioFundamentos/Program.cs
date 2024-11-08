678c974dbd5af8d4fff8d900f031f95cc86bd1e3
Loena Celeste <loenascimentosilva@gmail.com>
2024-11-08 10:26:56 -0300

    feat: add implementations, challenge complete

--- DesafioFundamentos/Program.cs
+++ DesafioFundamentos/Program.cs
@@ -3,11 +3,11 @@
 // Coloca o encoding para UTF8 para exibir acentuação
 Console.OutputEncoding = System.Text.Encoding.UTF8;
 
-decimal precoInicial = 0;
-decimal precoPorHora = 0;
+decimal precoInicial;
+decimal precoPorHora;
 
 Console.WriteLine("Seja bem vindo ao sistema de estacionamento!\n" +
-                  "Digite o preço inicial:");
+				  "Digite o preço inicial:");
 precoInicial = Convert.ToDecimal(Console.ReadLine());
 
 Console.WriteLine("Agora digite o preço por hora:");
@@ -22,38 +22,38 @@ bool exibirMenu = true;
 // Realiza o loop do menu
 while (exibirMenu)
 {
-    Console.Clear();
-    Console.WriteLine("Digite a sua opção:");
-    Console.WriteLine("1 - Cadastrar veículo");
-    Console.WriteLine("2 - Remover veículo");
-    Console.WriteLine("3 - Listar veículos");
-    Console.WriteLine("4 - Encerrar");
+	Console.Clear();
+	Console.WriteLine("Digite a sua opção:");
+	Console.WriteLine("1 - Cadastrar veículo");
+	Console.WriteLine("2 - Remover veículo");
+	Console.WriteLine("3 - Listar veículos");
+	Console.WriteLine("4 - Encerrar");
 
-    switch (Console.ReadLine())
-    {
-        case "1":
-            es.AdicionarVeiculo();
-            break;
+	switch (Console.ReadLine())
+	{
+		case "1":
+			es.AdicionarVeiculo();
+			break;
 
-        case "2":
-            es.RemoverVeiculo();
-            break;
+		case "2":
+			es.RemoverVeiculo();
+			break;
 
-        case "3":
-            es.ListarVeiculos();
-            break;
+		case "3":
+			es.ListarVeiculos();
+			break;
 
-        case "4":
-            exibirMenu = false;
-            break;
+		case "4":
+			exibirMenu = false;
+			break;
 
-        default:
-            Console.WriteLine("Opção inválida");
-            break;
-    }
+		default:
+			Console.WriteLine("Opção inválida");
+			break;
+	}
 
-    Console.WriteLine("Pressione uma tecla para continuar");
-    Console.ReadLine();
+	Console.WriteLine("Pressione uma tecla para continuar");
+	Console.ReadLine();
 }
 
 Console.WriteLine("O programa se encerrou");

1c79b4a13658d4f1da62f9a354171743b6b1bb85
EduardoDev0706 <eduardodev2026@gmail.com>
2025-06-16 13:54:46 -0300

    Adicao da funcao AdicionarVeiculo

--- DesafioFundamentos/Models/Estacionamento.cs
+++ DesafioFundamentos/Models/Estacionamento.cs
@@ -17,6 +17,10 @@ namespace DesafioFundamentos.Models
             // TODO: Pedir para o usuário digitar uma placa (ReadLine) e adicionar na lista "veiculos"
             // *IMPLEMENTE AQUI*
             Console.WriteLine("Digite a placa do veículo para estacionar:");
+            string placa = Console.ReadLine();
+            veiculos.Add(placa);
+
+            Console.WriteLine($"Veículo com placa '{placa}' adicionado com sucesso!");
         }
 
         public void RemoverVeiculo()

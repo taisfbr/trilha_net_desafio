aecad495fab96b84c1e5e54f949402ce5717aaf9
Glestman <ramonglestman2.0@gmail.com>
2025-06-20 15:10:48 -0300

    foi incrementado novas funcionalidades pendentes

--- DesafioFundamentos/Models/Estacionamento.cs
+++ DesafioFundamentos/Models/Estacionamento.cs
@@ -2,6 +2,7 @@ namespace DesafioFundamentos.Models
 {
     public class Estacionamento
     {
+  
         private decimal precoInicial = 0;
         private decimal precoPorHora = 0;
         private List<string> veiculos = new List<string>();
@@ -17,46 +18,61 @@ namespace DesafioFundamentos.Models
             // TODO: Pedir para o usuário digitar uma placa (ReadLine) e adicionar na lista "veiculos"
             // *IMPLEMENTE AQUI*
             Console.WriteLine("Digite a placa do veículo para estacionar:");
+            string novaPlaca = Console.ReadLine();
+            veiculos.Add(novaPlaca);
+            Console.WriteLine($"Veiculo com placa '{novaPlaca}' adicionado com sucesso");
         }
 
         public void RemoverVeiculo()
         {
             Console.WriteLine("Digite a placa do veículo para remover:");
-
+            string placaParaRemover = Console.ReadLine();
             // Pedir para o usuário digitar a placa e armazenar na variável placa
             // *IMPLEMENTE AQUI*
-            string placa = "";
+
 
             // Verifica se o veículo existe
-            if (veiculos.Any(x => x.ToUpper() == placa.ToUpper()))
+            if (veiculos.Any(x => x.ToUpper() == placaParaRemover.ToUpper()))
             {
+
                 Console.WriteLine("Digite a quantidade de horas que o veículo permaneceu estacionado:");
+                if (int.TryParse(Console.ReadLine(), out int horas))
+                {
 
-                // TODO: Pedir para o usuário digitar a quantidade de horas que o veículo permaneceu estacionado,
-                // TODO: Realizar o seguinte cálculo: "precoInicial + precoPorHora * horas" para a variável valorTotal                
-                // *IMPLEMENTE AQUI*
-                int horas = 0;
-                decimal valorTotal = 0; 
+                    // TODO: Pedir para o usuário digitar a quantidade de horas que o veículo permaneceu estacionado,
+                    // TODO: Realizar o seguinte cálculo: "precoInicial + precoPorHora * horas" para a variável valorTotal                
+                    // *IMPLEMENTE AQUI*
 
-                // TODO: Remover a placa digitada da lista de veículos
-                // *IMPLEMENTE AQUI*
+                    decimal valorTotal = (precoInicial + (precoPorHora * horas));
 
-                Console.WriteLine($"O veículo {placa} foi removido e o preço total foi de: R$ {valorTotal}");
-            }
-            else
-            {
-                Console.WriteLine("Desculpe, esse veículo não está estacionado aqui. Confira se digitou a placa corretamente");
+
+                    // TODO: Remover a placa digitada da lista de veículos
+                    veiculos.Remove(placaParaRemover);
+
+                    Console.WriteLine($"O veículo {placaParaRemover} foi removido e o preço total foi de: R$ {valorTotal:F2}");
+                }
+                else
+                {
+                    Console.WriteLine("Desculpe, esse veículo não está estacionado aqui. Confira se digitou a placa corretamente");
+                }
             }
         }
-
         public void ListarVeiculos()
         {
             // Verifica se há veículos no estacionamento
             if (veiculos.Any())
             {
                 Console.WriteLine("Os veículos estacionados são:");
-                // TODO: Realizar um laço de repetição, exibindo os veículos estacionados
-                // *IMPLEMENTE AQUI*
+                // Percorre a lista e exibe cada veículo
+                for (int i = 0; i < veiculos.Count; i++)
+                {
+                    Console.WriteLine($"Veículo {i + 1}: {veiculos[i]}");
+                }
+                // Outra forma de percorrer a lista (mais comum para Listas):
+                // foreach (string veiculo in veiculos)
+                // {
+                //     Console.WriteLine($"- {veiculo}");
+                // }
             }
             else
             {

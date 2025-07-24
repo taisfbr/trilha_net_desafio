62beb950f098fc0de8d344df535e84265860558f
Victor Sampaio <victor.sampaio2003@gmail.com>
2025-07-24 15:40:34 +0000

    feat: add "RemoverVeiculo()" and "ListarVeiculos()"

--- DesafioFundamentos/Models/Estacionamento.cs
+++ DesafioFundamentos/Models/Estacionamento.cs
@@ -41,7 +41,7 @@ namespace DesafioFundamentos.Models
 
             // Pedir para o usuário digitar a placa e armazenar na variável placa
             // *IMPLEMENTE AQUI*
-            string placa = "";
+            string? placa = Console.ReadLine()?.ToUpper();
 
             // Verifica se o veículo existe
             if (veiculos.Any(x => x.ToUpper() == placa.ToUpper()))
@@ -51,11 +51,14 @@ namespace DesafioFundamentos.Models
                 // TODO: Pedir para o usuário digitar a quantidade de horas que o veículo permaneceu estacionado,
                 // TODO: Realizar o seguinte cálculo: "precoInicial + precoPorHora * horas" para a variável valorTotal                
                 // *IMPLEMENTE AQUI*
-                int horas = 0;
-                decimal valorTotal = 0; 
+
+                // Capta a entrada do usuário, convertendo de string para inteiro
+                int horas = Convert.ToInt32(Console.ReadLine());;
+                decimal valorTotal = precoInicial + (precoPorHora * horas); 
 
                 // TODO: Remover a placa digitada da lista de veículos
                 // *IMPLEMENTE AQUI*
+                veiculos.Remove(placa);
 
                 Console.WriteLine($"O veículo {placa} foi removido e o preço total foi de: R$ {valorTotal}");
             }
@@ -73,6 +76,12 @@ namespace DesafioFundamentos.Models
                 Console.WriteLine("Os veículos estacionados são:");
                 // TODO: Realizar um laço de repetição, exibindo os veículos estacionados
                 // *IMPLEMENTE AQUI*
+
+                // Itera sobre cada veículo na lista, adicionando cada placa à variável 'veiculo' e depois exibe no console
+                foreach (var veiculo in veiculos)
+                {
+                    Console.WriteLine($"Veículo placa: {veiculo}");
+                }
             }
             else
             {

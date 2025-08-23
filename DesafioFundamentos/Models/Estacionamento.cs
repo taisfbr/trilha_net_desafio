55b0559cd8d3b6333787674b4d8913cf35c6d51f
Gabriel Ribeiro <gabrielribeiro.dev@outlook.com.br>
2025-08-23 15:30:57 -0300

    Feat: Adicionando uma funcionalidade para que as placas sejam padrão mercosul

--- DesafioFundamentos/Models/Estacionamento.cs
+++ DesafioFundamentos/Models/Estacionamento.cs
@@ -1,3 +1,8 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
 namespace DesafioFundamentos.Models
 {
     public class Estacionamento
@@ -8,33 +13,42 @@ namespace DesafioFundamentos.Models
 
         public Estacionamento(decimal precoInicial, decimal precoPorHora)
         {
-                this.precoInicial = precoInicial;
+            this.precoInicial = precoInicial;
             this.precoPorHora = precoPorHora;
         }
 
         public void AdicionarVeiculo()
         {
             Console.WriteLine("Digite a placa do veículo para estacionar: ");
-            string placa = Console.ReadLine();
-            veiculos.Add(placa);
+            string placa = Console.ReadLine().ToUpper();
+
+            if (ValidarPlacaMercosul(placa))
+            {
+                veiculos.Add(placa);
+                Console.WriteLine($"Veículo {placa} estacionado com sucesso!");
+            }
+            else
+            {
+                Console.WriteLine("Placa inválida! O padrão correto é Mercosul (ex: BRA2E19).");
+            }
         }
 
         public void RemoverVeiculo()
         {
             Console.WriteLine("Digite a placa do veículo para remover:");
+            string placa = Console.ReadLine().ToUpper();
 
-            string placa = Console.ReadLine();
-
-            if (veiculos.Any(x => x.ToUpper() == placa.ToUpper()))
+            if (veiculos.Any(x => x.ToUpper() == placa))
             {
-Console.WriteLine("Digite a quantidade de horas que o veículo permaneceu estacionado:");
-int horas;
-while (!int.TryParse(Console.ReadLine(), out horas) || horas < 0)
-{
-    Console.WriteLine("Por favor, digite um número válido de horas:");
-}
-decimal valorTotal = precoInicial + precoPorHora * horas;
-veiculos.Remove(placa);
+                Console.WriteLine("Digite a quantidade de horas que o veículo permaneceu estacionado:");
+                int horas;
+                while (!int.TryParse(Console.ReadLine(), out horas) || horas < 0)
+                {
+                    Console.WriteLine("Por favor, digite um número válido de horas:");
+                }
+
+                decimal valorTotal = precoInicial + precoPorHora * horas;
+                veiculos.Remove(placa);
 
                 Console.WriteLine($"O veículo {placa} foi removido e o preço total foi de: R$ {valorTotal}");
             }
@@ -58,6 +72,12 @@ veiculos.Remove(placa);
             {
                 Console.WriteLine("Não há veículos estacionados.");
             }
+        }
+
+        private bool ValidarPlacaMercosul(string placa)
+        {
+            string padrao = @"^[A-Z]{3}[0-9][A-Z][0-9]{2}$";
+            return Regex.IsMatch(placa, padrao);
         }
     }
 }
